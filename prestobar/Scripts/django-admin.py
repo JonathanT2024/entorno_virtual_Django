6ca2007027bfe4e0e76bb7cb08068d943d132416
JonathanT2024 <tolyn7@gmail.com>
2024-08-08 11:00:40 -0400

    Commit inicial en la rama main

---
+++ prestobar/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!G:\04.- Praxis\6.- M6\desafio 2\desafio2_jonthan_toloza\prestobar\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
